4393a35d67e050bf1a143acffd7b0a20f98c1edf
apirrone <awanegenebayo@gmail.com>
2025-09-01 13:52:50 +0200

    adding lookat head track demo

---
+++ examples/lookat_head_track_demo.py
@@ -0,0 +1,43 @@
+"""Reachy Mini Look At Head Tracking Example.
+
+Uses the lookat() api to have Reachy Mini follow the user's head movements.
+"""
+
+import cv2
+from head_tracker import HeadTracker
+
+from reachy_mini import ReachyMini
+from reachy_mini.utils.camera import find_camera
+
+cap = find_camera()
+assert cap is not None, (
+    "Could not find a camera. Please specify the camera index with --camera-opencv-id"
+)
+
+head_tracker = HeadTracker()
+
+with ReachyMini() as reachy_mini:
+    try:
+        while True:
+            success, img = cap.read()
+            if success:
+                eye_center, roll = head_tracker.get_head_position(img)
+                if eye_center is not None:
+                    h, w, _ = img.shape
+                    eye_center = (eye_center + 1) / 2
+                    eye_center[0] *= w
+                    eye_center[1] *= h
+                    cv2.circle(
+                        img,
+                        center=(int(eye_center[0]), int(eye_center[1])),
+                        radius=5,
+                        color=(0, 255, 0),
+                        thickness=2,
+                    )
+                    reachy_mini.look_at_image(*eye_center, duration=0.0)
+
+            cv2.imshow("image", img)
+
+            cv2.waitKey(1)
+    except KeyboardInterrupt:
+        pass
